023504f7fff5732906e32c7a565240ccf20f2fc2
Tahir Shah <fa24bsai15@ueas.edu.pk>
2026-08-26 22:45:41 +0500

    Fix logo path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ea580c', // Tailwind orange-600
     icons: [
       {
-        src: '/icon.png',
+        src: '/logo.png',
         sizes: 'any',
         type: 'image/png',
       },
